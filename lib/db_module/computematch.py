2ba2258a584b09387e09316167051548ac4bdde0
lcs2011cs <lcs2011cs@gmail.com>
2016-03-05 20:50:42 -0500

    modified db module

--- lib/db_module/computematch.py
+++ lib/db_module/computematch.py
@@ -1,31 +1,91 @@
-import sys
-from userModule import CUser,from postModule import CPost,from historyPostModule import CHistoryPost
+import sys, random, time
+from datetime import date
 from geopy.distance import vincenty
+
+from userModule import CUser
+from postModule import CPost
+from historyPostModule import CHistoryPost
+
+
 
 def calculateDistance(pointa, pointb):
     '''
         Desc: 
             Compute distance between pointa and pointb
         Args:
-            pointa = {}  
-        
-
+            pointa, pointb = {"city":..., "lat":..., "lon":...}
+        Ret:
+            return the distance(miles)
     '''
 
+    loca = (pointa["lat"],pointa["lon"])
+    locb = (pointb["lat"],pointb["lon"])
+    return (vincenty(loca, locb).miles)
 
-
+def getTimeDiff(time1,time2):
+    '''
+        Desc: 
+            Get time difference by offset seconds
+            
+        Args:
+            time: realtime - 1970.1.1
+        Ret:
+            two time's difference(minutes)
+    '''
+    timediff = (time1 - time2) / 60
+    return abs(timediff)
 
 
 def computeMatchPosts(uid, post_content):
 
 
 
-
 def computeMatchUsers(uid, post_content):
-
-
+    pass
 
 def giveSearchResult(uid, post_content):
+    match_list = {}
+
+    ploc = post_content["location"]
+    pt = post_content["time_range"]
+    ptype = post_content["type"]
+
+    if(ptype == None):
+        pass
+    else:
+        match_list["type"] = ptype
+
+    if(pt == None):
+        pass
+    else: 
+        st = post_content["time_range"]["start_time"]
+        en = post_content["time_range"]["end_time"]
+        match_list[]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- lib/db_module/messageModule.py
+++ lib/db_module/messageModule.py
@@ -1,5 +1,5 @@
 import sys, pymongo
-from .db import CDatabase
+from db import CDatabase
 
 class CMessage(CDatabase):
     def __init__(self):
