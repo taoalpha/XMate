be1f18e1e4ba5cf00e4365dc9885dd32e3081f2f
taoalpha <iamzhoutao92@gmail.com>
2016-05-04 00:17:42 +0000

    add data generator

--- dataGenerator/dataGenerator.py
+++ dataGenerator/dataGenerator.py
@@ -1,24 +1,87 @@
 import requests
+import random
+import userdata as DG
 
 postUrl = "http://localhost:5000"
 
-def sendUserProfile(profile):
-	r = requests.post(postUrl+'/user/', data = profile)
-	if (!checkStatus(r)):
-		break
+def sendUser(profile):
+    r = requests.post(postUrl+'/user/', data = profile)
+    if (r.json()["status"] != 1) :
+        raise Exception('Fail', 'Send User')
 
-def sendScheduleProfile(profile):
-	r = requests.post(postUrl+'/schedule/', data = profile)
+def sendPost(profile):
+    r = requests.post(postUrl+'/schedule/', data = profile)
+    if (r.json()["status"] != 1) :
+        raise Exception('Fail', 'Send Post')
+    return r.json()
 
-def sendMessageProfile(profile):
-	r = requests.post(postUrl+'/message/', data = profile)
+def sendMsg(profile):
+    r = requests.post(postUrl+'/message/', data = profile)
+    if (r.json()["status"] != 1) :
+        raise Exception('Fail', 'Send Msg')
+    return r.json()
 
 
-def checkStatus(r):
-	data = r.json()
-	if (data["status"] != 1):
-		print data
-		return false
-	else:
-		return true
-	
+# create some users and store all the fbids
+
+fbids = {}
+
+userNum = 10
+for i in range(userNum):
+    userData = DG.generateUser()
+    sendUser(userData)
+    fbids[userData['_id']] = {}
+    fbids[userData['_id']]["posts"] = []
+    fbids[userData['_id']]["msg"] = []
+
+
+# use a id from the fbids to create a post and store all the pids from return reponse
+
+pids = {}
+postNum = 10
+for i in range(postNum):
+    uid = (fbids.keys())[int(random.random()*userNum)]
+    post = DG.generatePost(uid)
+    # send schedule profile request and get the id
+    r = sendPost(post)
+    pids[r["_id"]] = {}
+    pids[r['_id']]["members"] = []
+    pids[r['_id']]["msg"] = []
+    # update fbdis._id.posts
+    fbids[uid]["posts"].append(r["_id"])
+
+# TODO: check correctness
+
+# use a uid generate some invite messages
+
+msgNum = 10
+for i in range(msgNum):
+    sid = (fbids.keys())[int(random.random()*userNum)]
+    rid = (fbids.keys())[int(random.random()*userNum)]
+    # post must be in sender's posts list
+    userPostLen = len(fbids[sid]["posts"])
+    if userPostLen > 0:
+        pid = fbids[sid]["posts"][int(random.random()*userPostLen)]
+        # generate msg
+        msg = DG.generateMsg(sid,rid,pid,"invite")
+        # send msg request
+        r = sendMsg(msg)
+        # store msgid
+        fbids[rid]["msg"].append(r["_id"])
+        pids[pid]["msg"].append(r["_id"])
+
+# use a uid generate some join messages
+for i in range(msgNum):
+    sid = (fbids.keys())[int(random.random()*userNum)]
+    rid = (fbids.keys())[int(random.random()*userNum)]
+    # join : post must be in receiver's posts list
+    userPostLen = len(fbids[sid]["posts"])
+    if userPostLen > 0:
+        pid = fbids[rid]["posts"][int(random.random()*userPostLen)]
+        # generate msg
+        msg = DG.generateMsg(sid,rid,pid,"join")
+        # send msg request
+        r = sendMsg(msg)
+        # store msgid
+        fbids[rid]["msg"].append(r["_id"])
+        pids[pid]["msg"].append(r["_id"])
---
+++ dataGenerator/userdata.py
@@ -0,0 +1,115 @@
+import random
+import string
+import moment
+
+U_FIELD = {
+    "_id" : "",
+    "username" : "",
+    "age" : "",
+    "gender" : "",
+    "preferred_gender" : "",
+    "city": "",
+    "credits" : "",
+    "latitude" : "",
+    "longitude" : "",
+    "last_time_login" : "",
+    "height" : "",
+    "weight" : "",
+    "schedule_list" : [],
+    "conflict_list" : [],
+    "history_partner" : [],
+    "history_events" : [],
+    "unprocessed_message" : []
+}
+
+def generateUser():
+    '''
+        generate user data
+        user data fields:
+        FIELDS = {
+            "_id" : "",
+            "username" : "",
+            "age" : "",
+            "gender" : "",
+            "preferred_gender" : "",
+            "city": "",
+            "credits" : "",
+            "latitude" : "",
+            "longitude" : "",
+            "last_time_login" : "",
+            "height" : "",
+            "weight" : "",
+            "schedule_list" : [],
+            "conflict_list" : [],
+            "history_partner" : [],
+            "history_events" : [],
+            "unprocessed_message" : []
+        }
+    '''
+    user = {}
+    user["_id"] = random.getrandbits(20)
+    user["username"] = random.choice(string.ascii_uppercase)+''.join(random.choice(string.ascii_lowercase) for _ in range(7)) +  " " + random.choice(string.ascii_uppercase)+''.join(random.choice(string.ascii_lowercase) for _ in range(7))
+    user["age"] = ''.join(random.choice(string.digits) for _ in range(2))
+    user["gender"] = ["male", "female"][(random.random() > 0.5)]
+    user["preferred_gender"] = ["male", "female"][(random.random() > 0.5)]
+    user["city"] = ''.join(random.choice(string.ascii_uppercase) for _ in range(2))
+    user["latitude"] = float(''.join(random.choice(string.digits) for _ in range(5))) / 1000
+    user["longitude"] = float(''.join(random.choice(string.digits) for _ in range(5))) / 1000
+    user["height"] = int(''.join(random.choice(string.digits) for _ in range(3))) % 180
+    user["weight"] = ''.join(random.choice(string.digits) for _ in range(2))
+    user["credits"] = int(random.random()*100)
+    # < 10 days to now
+    user["last_time_login"] = moment.now().epoch() - int(random.random()*86400*10)
+    for i in U_FIELD:
+        if i not in user:
+            user[i] = U_FIELD[i]
+    return user
+
+
+P_FIELD = {
+    "type":"",
+    "latitude":"",
+    "longitude":"",
+    "start_time":"",
+    "end_time":"",
+    "post_time":"",
+    "owner":"",
+    "member":[]
+}
+
+def generatePost(uid):
+    '''
+        Create a data for requesting to create a post
+        @param {string} uid - the user id associated ith this post
+    '''
+    post = {}
+    post["owner"] = uid
+    post["type"] = ["tennis","running","swimming","hiking","poker","movie","gym","game","concert","skating"][int(random.random()*10)]
+    post["latitude"] = float(''.join(random.choice(string.digits) for _ in range(5))) / 1000
+    post["longitude"] = float(''.join(random.choice(string.digits) for _ in range(5))) / 1000
+    # < 10 days from now
+    post["start_time"] = moment.now().epoch() + int(random.random()*86400*10)
+    # < 3 hours from start_time
+    post["end_time"] = post["start_time"] + int(random.random()*3*60*60)
+    # < 2 days to now
+    post["post_time"] = moment.now().epoch() - int(random.random()*86400*2)
+
+    for i in P_FIELD:
+        if i not in post:
+            post[i] = P_FIELD[i]
+    return post
+
+def generateMsg(sid, rid, pid, type):
+    '''
+        Generate message
+        @param {string} sid - sender_id
+        @param {string} rid - receiver_id
+        @param {string} pid - post_id
+        @param {string} type - msg type
+    '''
+    msg = {}
+    msg["sender_id"] = sid
+    msg["receiver_id"] = rid
+    msg["post_id"] = pid
+    msg["type"] = type
+    return msg
