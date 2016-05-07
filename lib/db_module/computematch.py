8c502f2f89dbc031c5233d3fa22fa344c779c599
lcs2011cs <lcs2011cs@gmail.com>
2016-05-07 16:45:12 -0400

    computematch

--- lib/db_module/computematch.py
+++ lib/db_module/computematch.py
@@ -1,15 +1,56 @@
-import sys, random, time
-from datetime import datetime
+import sys, random, time, hashlib
 import moment
 from geopy.distance import vincenty
 
+def getHash(post_content):
+    
+    strhash = ""
+    strhash += str(post_content["type"])
+    
+    st = moment.unix(post_content["start_time"])
+    strhash += str(st.month) + str(st.day) 
+
+    if(post_content["latitude"] == ""):
+        pass
+    else:
+        lat = '{:.1f}'.format(post_content["latitude"])
+        lon = '{:.1f}'.format(post_content["longitude"])
+        strhash += str(lat) + str(lon)
 
 
-def computeMatchPosts(uid, post_content, mydb):
+    return hashlib.md5(strhash).hexdigest()
 
-    dis_threshold = 2.0
+def Judge(cache_return):
+    t = chache_return["create_time"]
+    current_time = moment.now().epoch()
+    if(t - current_time > 3600):
+        return True
+    else:
+        return False
+
+
+def computeMatchPosts(post_content, mydb):
+
+    dis_threshold = 9.0
     docu_list = []
+    ood_flag = 0
 
+    #if the search has already been cached, return it
+    search_id = getHash(post_content)
+    id_list = []
+    id_list.append(search_id)
+    res = mydb.getData("cache",id_list)
+    if(res["status"] != 0):
+        if(res["status"] == 1):
+            outofdate = Judge(res["content"][0])
+            if(outofdate):
+                oodflag = 1
+                pass
+            else:
+                return res
+        else:
+            return res
+   
     id_list = []
     res = mydb.getData("schedule",id_list)
     if(res["status"] != 1):
@@ -18,30 +59,34 @@ def computeMatchPosts(uid, post_content, mydb):
 
     f1 = True
     f2 = True
-    if(post_content["time_range"] == None):
+    if(post_content["start_time"] == ""):
         f1 = False
-    if(post_content["latitude"] == None):
+    if(post_content["latitude"] == ""):
         f2 = False
 
-
+    #Find the match document list
     for doc in cursor:
+
         flag = True
-        if(post_content["type"] == None or post_content["type"] == doc["type"]):
-            if(post_content["time_range"] == None):
+        current_time = moment.now().epoch()
+        if(doc["start_time"] < current_time):
+            continue
+
+        if(post_content["type"] == "" or post_content["type"] == doc["type"]):
+            if(post_content["start_time"] == ""):
                 pass
             else:
-                ##########################modicfication!!!
                 st = moment.unix(post_content["start_time"])
                 nst = moment.date(st.year, st.month, st.day, 0).epoch()
-                if(doc["start_time"] > nst and doc["start_time"] < nst + 86400*1.5):
+                
+                if(doc["start_time"] > nst - 86400 and doc["start_time"] < nst + 86400*1.5):
                     doc["timediff"] = abs(doc["start_time"] - st)
-                    pass
                 else:
                     flag = False
         else:
             flag = False
         if(flag):
-            if(post_content["latitude"] == None):
+            if(post_content["latitude"] == ""):
                 pass
             else:
                 pointa = (doc["latitude"],doc["longitude"])
@@ -49,12 +94,12 @@ def computeMatchPosts(uid, post_content, mydb):
                 dist = vincenty(loca, locb).miles
                 if(dist < dis_threshold):
                     doc["diff"] = dist
-                    pass
                 else:
                     flag = False
 
         if(flag):
             docu_list.append(doc)
+    
 
     if(f1 and f2):
         docu_list.sort(key = lambda postd: (postd["time_diff"],postd["diff"]))
@@ -64,7 +109,23 @@ def computeMatchPosts(uid, post_content, mydb):
         else:
             docu_list.sort(key = lambda postd: postd["post_datetime"],reverse = True) 
     else:
-        docu_list.sort(key = lambda postd: postd["time_diff"]) 
+        docu_list.sort(key = lambda postd: postd["time_diff"])
+
+    #Insert the data into the cache
+    current_time = moment.now().epoch()
+    data_list = []
+    data = {"_id":search_id, "match_list":docu_list, "create_time":current_time}
+    data_list.append(data)
+    
+    if(oodflag == 0):
+        res = mydb.insertData("cache",data_list)
+        if(res["status"] != 1):
+            return res
+    else:
+        id_list = [search_id]
+        res = mydb.updateData("cache",id_list,data_list)
+        if(res["status"] != 1):
+            return res
 
     return returnHelper(content = docu_list)
 
@@ -72,48 +133,106 @@ def computeMatchPosts(uid, post_content, mydb):
 
 def computeMatchUsers(uid, pid, mydb):
 
-    #history user? the first version just use history schedule
-    recommend_user_list = set()
+    oodflag = 0 #out of date flag
+
+    #Check whether the same match has been in cache
+    strhash = str(uid)+str(pid)
+    match_id = hashlib.md5(strhash).hexdigest()
     id_list = []
-    id_list.append(uid)
+    id_list.append(match_id)
+    res = mydb.getData("cache",id_list)
+
+    if(res["status"] != 0):
+        if(res["status"] == 1):
+            outofdate = Judge(res["content"][0])
+            if(outofdate):
+                oodflag = 1
+                pass
+            else:
+                return res
+        else:
+            return res
+
+    recommend_user_list1 = set()
+    recommend_user_list2 = set()
+    #compute from schedule list
+    id_list = [pid]
+    res = mydb.getData("schedule",id_list)
+    if(res["status"] != 1):
+        return res
+    mpost = res["content"][0]
+
+    id_list = [uid]
     res = mydb.getData("user",id_list)
     if(res["status"] != 1):
         return res
-    user = res["content"][0]
-    history_events = user["history_events"]
+    history_events_list = res["content"][0]["history_events"]
 
-    #select suitable user from the current events
-    id_list = []
-    id_list.append(pid)
-    res = mydb.getData("schedule",id_list)
-    if(res["status"] != 1):
-        return res
-    doc = res["content"]
-    post_type = doc["type"]
 
     id_list = []
     res = mydb.getData("schedule",id_list)
     if(res["status"] != 1):
         return res
-    for doc in res["content"]:
-        if(doc["type"] == post_type):
-            for user_id in doc["member"]:
-                recommend_user_list.add(user_id)
+    cursor = res["content"]
 
-    #select users from history events
-    id_list = history_events
-    res = mydb.getData("history_schedule",id_list)
+    for doc in cursor:
+        if(doc["type"] == mpost["type"]):
+            if(doc["owner"] in history_events_list):
+                recommend_user_list1.add(doc["owner"])
+            else:
+                recommend_user_list2.add(doc["owner"])
+
+            for user_id in doc["member"]:
+                if(user_id in history_events_list):
+                    recommend_user_list1.add(user_id)
+                else:
+                    recommend_user_list2.add(user_id)
+
+    recommend_user_list = list(recommend_user_list1) + list(recommend_user_list2)
+    
+    #if recommendation set size is too small, give some random results
+    if(len(recommend_user_list) < 5):
+        id_list = []
+        res = mydb.getData("user",id_list)
+        if(res["status"] != 1):
+            return res
+        cursor = list(res["content"])
+        ranlist = random.sample(cursor,5)
+        recommend_user_list += ranlist
+
+    #get the user name from the recommended list
+    r_list = []
+    for i in range(0, len(recommend_user_list)):
+        data = {"_id":recommend_user_list[i]}
+        r_list.append(data)
+    
+    id_list = recommend_user_list
+    res = mydb.getData("user",id_list)
     if(res["status"] != 1):
         return res
-    for doc in res["content"]:
-        if(doc["type"] == post_type):
-            for user_id in doc["member"]:
-                recommend_user_list.add(user_id)
+    
+    cursor = res["content"]
+    for doc in cursor:
+        pos = recommend_user_list.index(doc["_id"])
+        r_list[pos]["username"] = doc["username"]
 
-    recommend_user_list = list(recommend_user_list)
-    return returnHelper(content = recommend_user_list)
+    #insert or update data into cache
+    current_time = moment.now().epoch()
+    data_list = []
+    data = {"_id":mactch_id, "match_list":r_list, "create_time":current_time}
+    data_list.append(data)
+    
+    if(oodflag == 0):
+        res = mydb.insertData("cache",data_list)
+        if(res["status"] != 1):
+            return res
+    else:
+        id_list = [mactch_id]
+        res = mydb.updateData("cache",id_list,data_list)
+        if(res["status"] != 1):
+            return res
 
-
+    return returnHelper(content = r_list)
 
 
 def returnHelper(status = 1, msg = None,content = None):
